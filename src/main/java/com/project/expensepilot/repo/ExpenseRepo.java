1aeb079be4f51101177c4587e61ad2d4e56ded50
Chetan <chetan87706@gmail.com>
2025-09-25 00:46:44 +0530

    adding Authentication

--- src/main/java/com/project/expensepilot/repo/ExpenseRepo.java
+++ src/main/java/com/project/expensepilot/repo/ExpenseRepo.java
@@ -6,4 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExpenseRepo extends JpaRepository<Expense, Integer> {
-}
+} 
---
+++ src/main/java/com/project/expensepilot/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.project.expensepilot.repo;
+
+import com.project.expensepilot.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<UserEntity, Integer> {
+
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
