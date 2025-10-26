19f46c7b8e9c2aaee2d1f77ed72048dd05b21860
gautam27-code <jaingautamcse27@gmail.com>
2025-10-26 22:05:11 +0530

    Work on AuthController
    Removed login method from AuthController.

--- src/main/java/com/project/expensepilot/controller/AuthController.java
+++ src/main/java/com/project/expensepilot/controller/AuthController.java
@@ -46,16 +46,8 @@ public class AuthController {
         this.jwtGenerator = jwtGenerator;
     }
 
-    @PostMapping("login")
-    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody LoginDto loginDto){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginDto.getUsername(),
-                        loginDto.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtGenerator.generateToken(authentication);
-        UserEntity user = userRepo.findByUsername(loginDto.getUsername()).orElseThrow();
-        return new ResponseEntity<>(new AuthResponseDTO(token, user), HttpStatus.OK);
-    }
+    //
+    
 
     @PostMapping("register")
     public ResponseEntity<?> register(@Valid @RequestBody RegisterDto registerDto){
