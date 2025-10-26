98817bd1aada4be907fb07e0411b41810f9a2f62
gautam27-code <jaingautamcse27@gmail.com>
2025-10-26 22:07:12 +0530

    Add password with validation to LoginDto

--- src/main/java/com/project/expensepilot/dto/LoginDto.java
+++ src/main/java/com/project/expensepilot/dto/LoginDto.java
@@ -9,6 +9,16 @@ public class LoginDto {
     private String username;
 
     //
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getPassword() {
         return password;
